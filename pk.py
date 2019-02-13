74f87be8f4d4ea777551ed0ac28c437071620d45
Zach Clare <zach7505@gmail.com>
2019-02-13 18:13:05 +1100

    Code commenting and cleanup - implementation of sphinx

--- pk.py
+++ pk.py
@@ -1,10 +1,7 @@
 # the main file for now
 
-import tkinter as tk  # May have to change this later to detect if tkinter is available
-
-import sys
 import functools
-from helpers import debug  # for debugging functions
+import tkinter as tk  # May have to change this later to detect if tkinter is available
 
 # Initialize a "controlling window"
 _master = tk.Tk()
@@ -12,7 +9,9 @@ _master.withdraw()  # Hide it for background control
 
 
 class Window(tk.Canvas):
-    # Class to manage the main window
+    """Class to manage window related actions
+
+    """
 
     def __init__(self):
         self.master = tk.Toplevel(_master)
@@ -25,8 +24,8 @@ class Window(tk.Canvas):
         # management of the "X" button (closing)
         self.master.protocol('WM_DELETE_WINDOW', self.on_close)
 
-    # Autoflush allows for the window to be updated automatically after a function call
     def autoflush(func):
+        # a decorator that refreshes the window once the decorated function is run - if autoflush is true
         @functools.wraps(func)
         def wrapper_decorator(*args, **kwargs):
             value = func(*args, **kwargs)
@@ -36,46 +35,83 @@ class Window(tk.Canvas):
 
         return wrapper_decorator
 
-    # stub to manage the window closing - should be modifiable by the user
     @autoflush
     def on_close(self):
+        """Stub to manage the closing of the window
+
+        Should be modifiable by the user
+        """
         self.master.destroy()
         # TODO check if window is destroyed and assert an error if so
 
-    # sets the background colour of the window
     @autoflush
-    def setBg(self, colour):
+    def set_bg(self, colour):
+        """Changes the background colour to the colour specified
+
+        :param colour: The colour to be changed to - accepts all tkinter colours
+        :type colour: string
+        """
         self.config(bg=colour)
+        # TODO implement rgb colours / class
 
 
 class Object:
+    """Class to manage base level attributes and commands of all objects
+
+    """
+
+    # TODO fix weird glitch where some parts of the shape don't draw if updated too quickly (about 40+ fps)
+    # noinspection PyShadowingNames
     def __init__(self, window):
+        """
+        :param window: window to bind the object to
+        :type window: pk.Window
+        """
         self.window = window
         self.id = None
         self.bbox_id = None
 
+    # a stub for drawing - different for different shapes
     def _draw(self):
-        pass # override this
+        pass  # overridden
 
     def draw(self, bbox=False):
-        # draw the shape
+        """Draws the shape to the current window
+
+        :param bbox: Whether to draw the bounding box or not
+        :type bbox: bool
+        """
         self.id = self._draw()
         if bbox:
             self.bbox_id = self.get_bbox()
 
         _master.update()
 
-    def draw_bbox(self, value):
-        if isinstance(value, bool):
-            self.bbox = value
-
+    # stub for getting the bbox - defined in BBox
     def get_bbox(self):
-        pass # override
+        pass  # overridden
 
 
+# noinspection PyCallByClass
 class BBox(Object):
-    # Bounding box for box-shaped objects
+    """A bounding box class that provides methods and management of box shaped objects"""
+
+    # TODO Add collision
+    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
+        """
+
+        :param window: The window the object is bound to
+        :type window: pk.Window
+        :param x: x coordinate of the object (from the top left corner)
+        :type x: int/float
+        :param y: y coordinate of the object (from the top left corner)
+        :type y: int/float
+        :param width: width of the object
+        :type width: int/float
+        :param height: height of the object
+        :type height: int/float
+        """
         self.x = float(x)
         self.y = float(y)
         self.width = float(width)
@@ -84,46 +120,75 @@ class BBox(Object):
         self.cy = self.y + (self.height / 2)
         Object.__init__(self, window)
 
+    # noinspection PyCallByClass
     def get_bbox(self):
-        return tk.Canvas.create_rectangle(self.window, self.x, self.y, self.width, self.height, outline='green', width='2')
+        """Returns the bounding box of the object in green
+
+        :return: Bounding box id
+        :rtype: int
+        """
+        return tk.Canvas.create_rectangle(self.window, self.x, self.y, self.width, self.height, outline='green',
+                                          width='2')
 
 
 class Line(BBox):
+    """Creates a straight line that starts from (x,y) and ends at width and height
+
+    (thinking about this, what kind of line has width and height....)
+    """
+
+    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         BBox.__init__(self, window, x, y, width, height)
 
+    # noinspection PyCallByClass
     def _draw(self):
         # tk uses x1 y1 x2 y2 notation, which is pretty gross
+        # TODO probably use x1 y1 x2 y2 because it makes more sense (apparently)....
         return tk.Canvas.create_line(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
 
 
 class Rectangle(BBox):
+    """Creates a rectangle that starts from (x,y) and is width long and height high"""
+
+    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         BBox.__init__(self, window, x, y, width, height)
 
+    # noinspection PyCallByClass
     def _draw(self):
         return tk.Canvas.create_rectangle(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
 
 
 class Circle(BBox):
+    """Creates a circle using (x,y) as the center point, that spreads radius outwards"""
+
+    # noinspection PyShadowingNames
     def __init__(self, window, x, y, radius):
         BBox.__init__(self, window, float(x) - radius, float(y) - radius, float(x) + radius, float(y) + radius)
 
+    # noinspection PyCallByClass
     def _draw(self):
+        # noinspection PyCallByClass
         return tk.Canvas.create_oval(self.window, self.x, self.y, self.width, self.height)
 
 
 class Oval(BBox):
+    """Creates an oval based on a defined box, which starts at (x,y) and is width long and height high"""
+
+    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         BBox.__init__(self, window, x, y, width, height)
 
+    # noinspection PyCallByClass
     def _draw(self):
         return tk.Canvas.create_oval(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
 
 
 if __name__ == '__main__':
     window = Window()
-    #window.setBg('red')
+    # window.set_bg('red')
+    # noinspection SpellCheckingInspection
     myline = Line(window, 100, 100, 100, 0)
     myline.draw(bbox=True)
     mycircle = Circle(window, 100, 100, 50)
