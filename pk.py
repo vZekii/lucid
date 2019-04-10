123e7fcc267f51073967c4a3220b610eed24947d
Zach Clare <zach7505@gmail.com>
2019-04-10 20:19:04 +1000

    Fully converted all objects to be polygon based, and added rotation. Cleaned up old comments.

--- pk.py
+++ pk.py
@@ -1,6 +1,7 @@
 # the main file for now
 
 import tkinter as tk  # May have to change this later to detect if tkinter is available
+import math
 
 # Initialize a "controlling window"
 _master = tk.Tk()
@@ -65,7 +66,6 @@ class Object:
     # ^^^ I'm assuming its to do with the refresh rate of the monitor
     # TODO make a better move function
     # TODO maybe implement a transformation class (rotation, sizing)
-    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         """
         :param window: window to bind the object to
@@ -87,6 +87,66 @@ class Object:
         self.width = width
         self.height = height
 
+        # Coordinates for all points of the shape
+        self.points = self.generate_points()
+
+        # options for customisation
+        self.options = {'outline': 'black',
+                        'fill': '',  # Empty string is transparent
+                        'smooth': 'false'}
+
+        self.rotation = 0  # recorded in degrees
+        self.precision = 4  # number of points that make up the shape
+
+    def generate_points(self):
+        # Converts xywh to (x1 y1) (x2 y2) ... (tk polygon format)
+        return [(self.x, self.y),
+                (self.x + self.width, self.y),
+                (self.x + self.width, self.y + self.height),
+                (self.x, self.y + self.height)]
+
+    def convert_points(self):
+        # converts points from x1 y1 x2 y2 to (x1, y1) (x2, y2) ...
+        self.points = [(self.points[i], self.points[i + 1]) for i in range(0, len(self.points), 2)]
+
+    def rotate(self, angle):
+        self.rotation += angle
+        if self.rotation >= 360: self.rotation -= 360
+        rotation = self.rotation * math.pi / 180.0
+
+        # major and minor axes
+        a = self.width / 2
+        b = self.height / 2
+
+        # center
+        xc = self.x + a
+        yc = self.y + b
+
+        point_list = []
+
+        # create the oval as a list of points
+        for i in range(self.precision):
+            # Calculate the angle for this step
+            # 360 degrees == 2 pi radians
+            theta = (math.pi * 2) * (float(i) / self.precision)
+
+            x1 = a * math.cos(theta)
+            y1 = b * math.sin(theta)
+
+            # rotate x, y
+            x = (x1 * math.cos(rotation)) + (y1 * math.sin(rotation))
+            y = (y1 * math.cos(rotation)) - (x1 * math.sin(rotation))
+
+            point_list.append(round(x + xc))
+            point_list.append(round(y + yc))
+
+        self.points = point_list
+
+        if self.id is not None:
+            self.window.coords(self.id, *self.points)
+
+        self.convert_points()
+
     # a stub for drawing - different for different shapes
     def _draw(self): return 0  # overridden
 
@@ -96,6 +156,13 @@ class Object:
         self.id = self._draw()
         _master.update()
 
+    def draw_points(self):
+        # debug function that shows points of object as red and border box as green
+        tk.Canvas.create_rectangle(self.window, self.x, self.y,
+                                   self.x + self.width, self.y + self.height, outline='green', width='2')
+        for x, y in self.points:
+            tk.Canvas.create_line(self.window, x, y, x+1, y+1, width='5', fill='red')
+
 
 class Line(Object):
     """Creates a straight line that starts from (x,y) and ends at width and height
@@ -103,52 +170,51 @@ class Line(Object):
     (thinking about this, what kind of line has width and height....)
     """
 
-    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         Object.__init__(self, window, x, y, width, height)
 
-    # noinspection PyCallByClass
     def _draw(self):
         # tk uses x1 y1 x2 y2 notation, which is pretty gross
         # TODO probably use x1 y1 x2 y2 because it makes more sense (apparently)....
-        return tk.Canvas.create_line(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
+        return Window.create_line(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
 
 
 class Rectangle(Object):
     """Creates a rectangle that starts from (x,y) and is width long and height high"""
 
-    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         Object.__init__(self, window, x, y, width, height)
 
-    # noinspection PyCallByClass
-    def _draw(self):
-        return tk.Canvas.create_rectangle(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
+    def _draw(self): return Window.create_polygon(self.window, self.points, self.options)
 
 
 class Circle(Object):
     """Creates a circle using (x,y) as the center point, that spreads radius outwards"""
+    # TODO maybe make it also availble to put in bounding box?
+    # TODO make a functiom that returns a bbox from a center point
 
-    # noinspection PyShadowingNames
     def __init__(self, window, x, y, radius):
-        Object.__init__(self, window, float(x) - radius, float(y) - radius, float(x) + radius, float(y) + radius)
+        Object.__init__(self, window, float(x) - radius, float(y) - radius, radius*2, radius*2)
+        self.options['smooth'] = True
+        self.precision = 30  # Makes the circle more rounded
+        self.rotate(0)
+        # TODO make this more efficient
 
-    # noinspection PyCallByClass
-    def _draw(self):
-        # noinspection PyCallByClass
-        return tk.Canvas.create_oval(self.window, self.x, self.y, self.width, self.height)
+    def _draw(self): return Window.create_polygon(self.window, self.points, self.options)
 
 
 class Oval(Object):
     """Creates an oval based on a defined box, which starts at (x,y) and is width long and height high"""
 
-    # noinspection PyShadowingNames
     def __init__(self, window, x, y, width, height):
         Object.__init__(self, window, x, y, width, height)
+        self.options['smooth'] = True
+        self.precision = 20
+        self.rotate(0)
 
-    # noinspection PyCallByClass
     def _draw(self):
-        return tk.Canvas.create_oval(self.window, self.x, self.y, self.x + self.width, self.y + self.height)
+        # TODO Fix the redundancy of the polygon draw statements, without affecting text and entry
+        return Window.create_polygon(self.window, self.points, self.options)
 
 
 class Text(Object):
@@ -185,23 +251,24 @@ class Entry(Object):
 if __name__ == '__main__':
     window = Window()
     # window.set_bg('red')
-    # noinspection SpellCheckingInspection
-    myline = Line(window, 100, 100, 100, 0)
+    myline = Line(window, 250, 250, 100, 0)
     myline.draw()
-    mycircle = Circle(window, 100, 100, 50)
+    mycircle = Circle(window, 250, 250, 200)
     mycircle.draw()
+    #mycircle.draw_points()
     myrect = Rectangle(window, 0, 0, 200, 200)
-    myrect.draw()
+    #myrect.draw()
     myoval = Oval(window, 0, 50, 200, 100)
     myoval.draw()
+    myoval.draw_points()
     mytext = Text(window, 50, 50, 'YEET')
     mytext.draw()
     myentry = Entry(window, 100, 100, 20)
     myentry.draw()
 
     while True:
-        window.move(myoval.id, 1, 1)
-        window.move(mycircle.id, 1, 1)
+        # window.move(myoval.id, 1, 1)
+        # window.move(mycircle.id, 1, 1)
         window.update_idletasks()
         window.update()
         window.after(15)
