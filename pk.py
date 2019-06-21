238f5138dae6e72dc78e9325e8c9b9d19fac282f
zach7 <zach7505@gmail.com>
2019-06-21 23:23:16 +1000

    Added Menus and Exceptions, Fixed circular rounding, and general bug fixing

--- pk.py
+++ pk.py
@@ -3,7 +3,16 @@
 import math
 import string
 import sys
+from collections import namedtuple
 import tkinter as tk  # May have to change this later to detect if tkinter is available
+
+MenuItem = namedtuple('MenuItem', ['label', 'command'])
+
+
+class PkError(Exception):
+    """Base class for all exceptions"""
+    pass
+
 
 # Initialize a "controlling window"
 _master = tk.Tk()
@@ -31,17 +40,26 @@ class Window(tk.Canvas):
         # start the event handler
         self.EventHandler = EventHandler(self)
 
+        # set up default menu
+        self.menu = Menu(self)
+        self.master.config(menu=self.menu)
+
     def on_close(self):
         """Stub to manage the closing of the window - should be modifiable by the user"""
         pass
 
-    def _on_close(self):
+    def _on_close(self, *args):
         """runs the user-defined close function, then closes then window"""
         self.on_close()
         self.master.destroy()
-        sys.exit()   # Actually closes the window now
+        sys.exit()  # Actually closes the window now
 
-    def set_bg(self, colour):  # TODO change this to a property
+    @property
+    def bg(self):
+        return self.cget('bg')
+
+    @bg.setter
+    def bg(self, colour):
         """Changes the background colour to the colour specified
 
         :param colour: The colour to be changed to - accepts all tkinter colours, hex, and conversions from rgb
@@ -54,7 +72,7 @@ class Window(tk.Canvas):
             self.EventHandler.bindings[key] = func
             # TODO somehow get arguments without lambda
         else:
-            raise Exception(f'Key not in bindings dir: {key}')
+            raise PkError(f'Key not found in bindings dir: {key}')
 
     def get_mouse(self):
         """Returns the current cursor position based on the window
@@ -66,6 +84,31 @@ class Window(tk.Canvas):
         return x, y
 
     # TODO look at making a Coords and Bbox named tuple for ease
+
+
+class Menu(tk.Menu):
+    current_menu = ''
+
+    def __init__(self, master):
+        tk.Menu.__init__(self, master)
+
+        self.submenus = {}
+        self.add_cascade(label='Quit', command=master.quit)
+
+        self.current_menu = self
+
+    def add_submenu(self, name, menulist):
+        submenu = self._create_submenu()
+        for item in menulist:
+            if isinstance(item, MenuItem):
+                submenu.add_command(label=item.label, command=item.command)
+            else:
+                submenu.add_separator()
+        self.submenus[name] = submenu
+        self.add_cascade(label=name, menu=submenu)
+
+    def _create_submenu(self):
+        return tk.Menu(self.current_menu, tearoff=0)
 
 
 class EventHandler:
@@ -135,7 +178,6 @@ class Object:
 
     # TODO fix weird glitch where some parts of the shape don't draw if updated too quickly (about 40+ fps)
     # ^^^ I'm assuming its to do with the refresh rate of the monitor
-    #  TODO implement sizing (maybe)
     def __init__(self, window, x, y, width, height, options=None):
         """
         :param window: window to bind the object to
@@ -163,19 +205,19 @@ class Object:
         }
 
         self.options = {
-                'fill': '',  # Empty string is transparent
-                'outline': 'black',
-                'width': 1,  # this is outline thickness
-                'smooth': False,
+            'fill': '',  # Empty string is transparent
+            'outline': 'black',
+            'width': 1,  # this is outline thickness
+            'smooth': False,
         }
+
+        self.rotation = 0  # recorded in degrees
+        self.precision = 4  # number of points that make up the shape
 
         if options: self.options.update(options)
 
         # Coordinates for all points of the shape
         self.points = self.generate_points()
-
-        self.rotation = 0  # recorded in degrees
-        self.precision = 4  # number of points that make up the shape
 
         if window.autodraw: self.draw()
 
@@ -185,13 +227,15 @@ class Object:
 
     # This is how the option setting would work
     def _optionset(self, opt, val):
+        self.options[opt] = val
         if self.id:  # only change it immediately if the object is drawn
             self.window.itemconfigure(self.id, {opt: val})
 
     def _propertyset(self):
         if self.id:
             self.points = self.generate_points()
-            self.rotate_to(self.rotation)  # allows the shape to keep rotation - this causes the center point to change however, causing issues
+            self.rotate_to(
+                self.rotation)  # allows the shape to keep rotation - this causes the center point to change however (is this a bug?)
             if not self.rotation:
                 self._updatecenter()  # only lets the center point change when rotation is 0
             self.window.coords(self.id, self.convert_points_line())
@@ -203,7 +247,22 @@ class Object:
     @fill.setter
     def fill(self, value):
         self._optionset('fill', value)
-        self.options['fill'] = value
+
+    @property
+    def outline(self):
+        return self.options['outline']
+
+    @outline.setter
+    def outline(self, value):
+        self._optionset('outline', value)
+
+    @property
+    def thickness(self):
+        return self.options['width']
+
+    @thickness.setter
+    def thickness(self, value):
+        self._optionset('width', value)
 
     @property
     def x(self):
@@ -251,10 +310,15 @@ class Object:
 
     def generate_points(self):
         # Converts xywh to (x1 y1) (x2 y2) ... (tk polygon format)
-        return [(self.x, self.y),
-                (self.x + self.width, self.y),
-                (self.x + self.width, self.y + self.height),
-                (self.x, self.y + self.height)]
+        default = [(self.x, self.y),
+                   (self.x + self.width, self.y),
+                   (self.x + self.width, self.y + self.height),
+                   (self.x, self.y + self.height)]
+
+        if self.precision != 4:
+            return self.generate_curve_points()
+        else:
+            return default
 
     def convert_points(self):
         # converts points from x1 y1 x2 y2 to (x1, y1) (x2, y2) ...
@@ -264,8 +328,29 @@ class Object:
         """ converts points from (x1, y1) (x2, y2) to x1 y1 x2 y2 """
         out = []
         for point in self.points:
-            out.append(point[0]); out.append(point[1])
+            out.append(point[0]);
+            out.append(point[1])
         return out
+
+    def generate_curve_points(self):
+        """ function to generate circles and ovals from a bbox"""
+
+        # major and minor axes
+        a = self.width / 2
+        b = self.height / 2
+
+        point_list = []
+
+        for i in range(self.precision):
+            theta = (math.pi * 2) * (float(i) / self.precision)
+
+            x = a * math.cos(theta)
+            y = b * math.sin(theta)
+
+            point_list.append(round(x + self.cx))
+            point_list.append(round(y + self.cy))
+
+        return point_list
 
     def _rotate(self):
         """internal command to control rotations - shared by rotate and rotate_to
@@ -281,12 +366,9 @@ class Object:
         # Convert the angle into radians? for math stuff
         theta = self.rotation * math.pi / 180.0  # fixed bug where self.rotation was being used instead of angle
 
-        # get the center point of the object - may replace this with a function
-        cx = self.x + (self.width / 2)
-        cy = self.y + (self.height / 2)
-
         new_points = []
         self.points = self.generate_points()  # Resets the object back to default, to allow more precise rotates
+        self.convert_points()
 
         for (px, py) in self.points:
             # move object point back to origin
@@ -324,8 +406,8 @@ class Object:
         return Window.create_polygon(self.window, self.points, self.options)
 
     def draw(self):
-        """Draws the shape to the current window """
-        if self.id is None:
+        """Draws the shape to the current window - nothing happens if it is already drawn"""
+        if not self.is_drawn:
             self.id = self._draw()
             _master.update()
 
@@ -344,20 +426,39 @@ class Object:
 
         # debug function that shows points of object as red and border box as green
         Window.create_rectangle(self.window, self.x, self.y, self.x + self.width, self.y + self.height, outline='green',
-                                width='2', tag=self.debug_tag)
+                                width=2, tag=self.debug_tag)
         for x, y in self.points:
-            Window.create_line(self.window, x, y, x + 1, y + 1, width='5', fill='red', tag=self.debug_tag)
+            Window.create_line(self.window, x, y, x + 1, y + 1, width=5, fill='red', tag=self.debug_tag)
+
+    @property
+    def is_drawn(self):
+        if self.id:
+            return True
+        else:
+            return False
 
     # function to move the object
     def move(self, x, y):
-        if self.id:  # TODO change this to an is_drawn function or something
+        if self.is_drawn:
             self.x += x
             self.y += y
             if self.debug_tag:
                 self.window.move(self.debug_tag, x, y)
             self.window.move(self.id, x, y)
         else:
-            raise Exception('Object not currently drawn')  # TODO make some custom exceptions
+            raise PkError('Object not currently drawn')
+
+    def __repr__(self):
+        return "Object {}".format(self.__class__)
+
+    def collideswith(self, obj2):
+        if Object in obj2.__class__.__mro__:
+            check = not ((obj2.x > (self.x + self.width) or (obj2.x + obj2.width) < self.x) or (
+                        obj2.y < (self.y + self.height)) or ((obj2.y + obj2.height) > self.y))
+
+            return check
+        else:
+            raise PkError(obj2.__class__, 'is not a pk.Object')
 
 
 class Line(Object):
@@ -383,9 +484,12 @@ class Circle(Object):
     # TODO maybe make it also availble to put in bounding box?
     # TODO make a functiom that returns a bbox from a center point
 
-    def __init__(self, window, x, y, radius):
-        Object.__init__(self, window, float(x) - radius, float(y) - radius, radius * 2, radius * 2)
-        self.options['smooth'] = True
+    def __init__(self, window, x, y, radius, **options):
+        if options:
+            options.update({'smooth': True})
+        else:
+            options = {'smooth': True}
+        Object.__init__(self, window, float(x) - radius, float(y) - radius, radius * 2, radius * 2, options=options)
         self.precision = 30  # Makes the circle more rounded
         self.rotate(0)
         # TODO make this more efficient
@@ -394,9 +498,12 @@ class Circle(Object):
 class Oval(Object):
     """Creates an oval based on a defined box, which starts at (x,y) and is width long and height high"""
 
-    def __init__(self, window, x, y, width, height):
-        Object.__init__(self, window, x, y, width, height)
-        self.options['smooth'] = True
+    def __init__(self, window, x, y, width, height, **options):
+        if options:
+            options.update({'smooth': True})
+        else:
+            options = {'smooth': True}
+        Object.__init__(self, window, x, y, width, height, options=options)
         self.precision = 20
         self.rotate(0)
 
@@ -497,7 +604,6 @@ def rgb(red, green, blue):
             raise ValueError(f'Value {c} not within 0-255')
 
     return f'#{red:02x}{green:02x}{blue:02x}'
-
 
 # TODO Implement listboxes, radiobuttons (maybe)
 # TODO Implement Menubars (File, Edit, etc)
